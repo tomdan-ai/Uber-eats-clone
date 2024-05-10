bb6c91d98ace5f9a4d2184f5ee161d49d2211dc6
tomdan-ai <tomudoh258@gmail.com>
2024-05-10 11:07:42 +0100

    Module and service logic

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LocalStrategy } from './local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
+// import { AuthController } from './auth.controller';
 
 @Module({
-  providers: [AuthService]
+  imports: [
+    JwtModule.register({
+      secret: '',
+      signOptions: { expiresIn: '1h' }
+    }),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  exports: [AuthService]
 })
 export class AuthModule {}
