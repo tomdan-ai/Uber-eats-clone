0fb96836b724801e67c93e908690af1b5cb8ceca
tomdan-ai <tomudoh258@gmail.com>
2024-05-10 11:28:03 +0100

    Setup ENV for JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -8,7 +8,7 @@ import { JwtStrategy } from './jwt.strategy';
 @Module({
   imports: [
     JwtModule.register({
-      secret: '',
+      secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '1h' }
     }),
   ],
