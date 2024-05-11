69fc876c4fab73adf433fa56a645e2d88c3fbfa9
tomdan-ai <tomudoh258@gmail.com>
2024-05-11 21:22:11 +0100

    Install and import nest config service

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -2,7 +2,7 @@ import { Strategy, ExtractJwt } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { AuthService } from './auth.service';
-
+import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private authService: AuthService) {
