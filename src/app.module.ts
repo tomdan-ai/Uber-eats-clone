722acb112675b085cc22770123a07d54b5db79a2
tomdan-ai <tomudoh258@gmail.com>
2024-05-11 22:34:13 +0100

    Intergrate Uauth API to main Application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,32 @@
+// import { Module } from '@nestjs/common';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+// import { ConfigModule } from '@nestjs/config'; // Import ConfigModule
+// import { AuthModule } from './auth/auth.module';
+
+// @Module({
+//   imports: [
+//     ConfigModule.forRoot(), // Import ConfigModule to access environment variables
+//     AuthModule
+//   ],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+// export class AppModule {}
+
+
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(), // Import ConfigModule to access environment variables
+    AuthModule,
+  ],
+  controllers: [AppController], // Include AppController
+  providers: [AppService], // Include AppService
 })
 export class AppModule {}
