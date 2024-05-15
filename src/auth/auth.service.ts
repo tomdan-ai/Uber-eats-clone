b9627d3cce21aae9156a5be5b33c3a5016a09349
tomdan-ai <tomudoh258@gmail.com>
2024-05-15 19:45:44 +0100

    Add LOgger for error Handling

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,32 @@
 // auth/auth.service.ts
 
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './user.model';
 
 @Injectable()
 export class AuthService {
+  private readonly logger = new Logger(AuthService.name)
   constructor(@InjectModel(User.name) private readonly userModel: Model<User>) {}
 
   async validateUser(email: string, password: string): Promise<any> {
+    this.logger.log(`Validating user with email: ${email}`); // Log at the "log" level
+  
     const user = await this.userModel.findOne({ email }).exec();
-    if (!user || user.password !== password) {
+    if (!user) {
+      this.logger.warn(`User not found with email: ${email}`); // Log at the "warn" level
       return null;
     }
+  
+    if (user.password !== password) {
+      this.logger.error(`Invalid password for user: ${user.email}`); // Log at the "error" level
+      return null;
+    }
+  
+    this.logger.debug(`User authenticated: ${user.email}`); // Log at the "debug" level
+  
     return user;
   }
 
