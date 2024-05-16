6fbd9c14d33a84cce0e91e528f2f4b618d55778b
tomdan-ai <tomudoh258@gmail.com>
2024-05-16 17:50:03 +0100

    Set up Redux for state management

---
+++ frontend/src/redux/reducers/index.js
@@ -0,0 +1,11 @@
+// my-react-app/src/redux/reducers/index.js
+import { combineReducers } from 'redux';
+// Import your reducers here
+// import authReducer from './authReducer';
+
+const rootReducer = combineReducers({
+  // auth: authReducer,
+  // Add other reducers here
+});
+
+export default rootReducer;
