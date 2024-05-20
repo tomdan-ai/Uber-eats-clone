d218df7c1063eb2720a91a1bab42150a2abaec44
tomdan-ai <tomudoh258@gmail.com>
2024-05-20 23:16:59 +0100

    Enabling CORS in application

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
+// main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: ['error', 'warn'], // Show only error and warn logs
+  const app = await NestFactory.create(AppModule);
+
+  // Enable CORS
+  app.enableCors({
+    origin: 'http://localhost:5173', // The URL of your frontend application
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
   });
-  await app.listen(3000);
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
