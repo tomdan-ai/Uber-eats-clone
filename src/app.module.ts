1f38ef2c48c8691219d03c65fc616073ac52af7e
tomdan-ai <tomudoh258@gmail.com>
2024-05-24 08:25:22 +0100

    Import and configure resturant module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,22 @@
-// app.module.ts
+// src/app.module.ts
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { RestaurantModule } from './restaurants/restaurant.model';
 import { User, UserSchema } from './auth/user.model';
+import { Restaurant, RestaurantSchema } from './restaurants/schemas/restaurant.schema';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGODB_URI),
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: Restaurant.name, schema: RestaurantSchema },
+    ]),
     AuthModule,
+    RestaurantModule,
   ],
 })
 export class AppModule {}
