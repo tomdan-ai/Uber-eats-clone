963f2a602797641e3ddb4588fe863e753537f133
tomdan-ai <tomudoh258@gmail.com>
2024-05-27 12:04:35 +0100

    Write restaurant logic

---
+++ src/restaurants/restaurant.model.ts
@@ -0,0 +1,16 @@
+// src/restaurant/restaurant.module.ts
+
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Restaurant, RestaurantSchema } from './schemas/restaurant.schema';
+import { RestaurantService } from './restaurant.service';
+import { RestaurantController } from './restaurant.controller';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Restaurant.name, schema: RestaurantSchema }]),
+  ],
+  controllers: [RestaurantController],
+  providers: [RestaurantService],
+})
+export class RestaurantModule {}
